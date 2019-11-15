9d930ce206a3b8269fa7b080f7977a3c878c1edd
Gonzalo Quinteros <chonz0@gmail.com>
2019-11-15 20:42:29 -0300

    Use objects instead of strings as payload

--- App.js
+++ App.js
@@ -15,39 +15,41 @@ import ReconnectingWebSocket from 'react-native-reconnecting-websocket';
 let ws;
 
 const App = () => {
-  const [color, setColor] = useState(null);
+  const [color, setColor] = useState('');
   const [lastMessage, setLastMessage] = useState('');
 
   useEffect(() => {
-    // ws = new WebSocket('ws://localhost:8999');
-    // ws = new WebSocket('ws://localhost:5000');
+    // ws = new ReconnectingWebSocket('ws://localhost:8999');
+    // ws = new ReconnectingWebSocket('ws://localhost:5000');
     // ws = new WebSocket('ws://colorful-server.herokuapp.com');
     ws = new ReconnectingWebSocket('ws://colorful-server.herokuapp.com');
 
     ws.onopen = () => {
-      // connection opened
       console.log('onopen');
       ws.send('hi from react native'); // send a message
     };
 
-    ws.onmessage = (e) => {
+    ws.onmessage = (msg) => {
       // a message was received
-      console.log('onmessage', e, e.data);
-      if (e && e.data) {
-        setLastMessage(e.data);
-        setColor(e.data);
+      console.log('onmessage', msg, msg.data);
+      if (msg && msg.data) {
+        const { type, payload } = JSON.parse(msg.data);
+
+        switch(type) {
+          case 'color':
+            setColor(payload);
+            break;
+
+          case 'status':
+          default:
+            setLastMessage(payload);
+            break;
+        }
       }
     };
 
-    ws.onerror = (e) => {
-      // an error occurred
-      console.log('onerror', e.message);
-    };
-
-    ws.onclose = (e) => {
-      // connection closed
-      console.log('onclose', e.code, e.reason);
-    };
+    ws.onerror = (e) => console.log('onerror', e.message);
+    ws.onclose = (e) => console.log('onclose', e.code, e.reason);
   }, []);
 
   const getColor = () => {
@@ -55,32 +57,44 @@ const App = () => {
     ws.send('getColor');
   }
 
+  const styles = StyleSheet.create({
+    loading: {
+      alignItems: 'center',
+      alignContent: 'space-between',
+      justifyContent: 'center',
+      flex: 1
+    },
+    touchable: {
+      alignItems: 'center',
+      alignContent: 'space-between',
+      justifyContent: 'center',
+      flex: 1
+    },
+    // body: {
+    //   backgroundColor: color,
+    // },
+  });
+
   return (
     <>
       <SafeAreaView style={{backgroundColor: color, flex: 1}}>
           <View style={{flex: 1}}>
-            {color === null && <Text style={{alignItems: 'center', alignContent: 'space-between', alignItems: 'center', justifyContent: 'center', flex: 1}}>Loading...</Text>}
+            <Text style={{alignSelf: 'center', color: 'rgba(0, 0, 0, 0.5)'}}>{lastMessage}</Text>
+            {lastMessage === '' && <Text style={styles.loading}>Loading...</Text>}
             {
-              color !== null &&
-              <TouchableOpacity style={{alignItems: 'center', alignContent: 'space-between', alignItems: 'center', justifyContent: 'center', flex: 1}} onPress={getColor}>
+              <TouchableOpacity style={styles.touchable} onPress={getColor}>
                 <Text style={{fontSize: 40}}>🎨</Text>
               </TouchableOpacity>
             }
-            <Text style={{alignSelf: 'flex-end', color: '#00f'}}>{lastMessage}</Text>
+            <Text style={{alignSelf: 'center', color: 'rgba(0, 0, 0, 0.5)'}}>{color}</Text>
           </View>
       </SafeAreaView>
     </>
   );
 };
 
-// const styles = StyleSheet.create({
-//   body: {
-//     backgroundColor: color,
-//   },
-// });
-
 const codePushOptions = {
-  checkFrequency: codePush.CheckFrequency.ON_APP_START
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME
 };
 
 export default codePush(codePushOptions)(App);
